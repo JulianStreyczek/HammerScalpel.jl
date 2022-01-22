6c568a0246414915ad823a3c91230047ec48b0ad
JulianStreyczek <82290284+JulianStreyczek@users.noreply.github.com>
2022-01-22 12:26:21 +0100

    'fix unit tests, attempt 2'

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test, HammerScalpel
 function solveModelTest()
     
     # Solve model with standard parameters
-    @time noint    = nopolicy(TT=2, ndims=3);                     # no intervention at all
+    @time noint    = nopolicy(TT=2);                     # no intervention at all
     @time notest   = withpolicy("notest", TT=2, ndims=3);         # no testing 
     @time untarget = withpolicy("untargettest", TT=2, ndims=3);   # untargeted testing
     @time target   = withpolicy("targettest", TT=2, ndims=3);     # targeted testing
