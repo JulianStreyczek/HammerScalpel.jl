b15c50e7a460ea38748afbc87769022b115841ed
JulianStreyczek <82290284+JulianStreyczek@users.noreply.github.com>
2022-01-22 16:17:28 +0100

    minor updates to solveModel.jl

--- src/solveModel.jl
+++ src/solveModel.jl
@@ -8,7 +8,7 @@ The function calls [`nopolicy`](@ref) and [`withpolicy`](@ref) with the relevant
 [`createFig5`](@ref), [`createFig6`](@ref), and [`createFig7`](@ref).    
 
 # Arguments:
-- `nprocs::Int64`: Optional. If specified, uses the module [Distributed](https://docs.julialang.org/en/v1/manual/distributed-computing/) for parallelizing computations. 
+- `nprocs::Int64`: Optional. If specified, uses the module [Distributed](https://docs.julialang.org/en/v1/manual/distributed-computing/) for parallelizing computations among `nprocs` workers. 
 - `TT::Integer`: Number of periods.
 - `ndims::Integer`: Number of grid points in each dimension.
 - `thetai::Float`: Probability that infected person sends "infected" signal.
@@ -18,7 +18,7 @@ function solveModel(; TT=52, ndims=20, thetai=0.38, thetas=0.0044)
     println("Solving the model under no policy, and under policies 'notest', 'untarget', 'target', 'isolate'. This may take a while...")
     
     # Solve model with standard parameters
-    @time noint    = nopolicy(TT);                                            # no intervention at all
+    @time noint    = nopolicy(;TT);                                            # no intervention at all
     @time notest   = withpolicy("notest"; TT, ndims, thetai, thetas);         # no testing 
     @time untarget = withpolicy("untargettest"; TT, ndims, thetai, thetas);   # untargeted testing
     @time target   = withpolicy("targettest"; TT, ndims, thetai, thetas);     # targeted testing
@@ -48,7 +48,7 @@ function solveModel(nprocs::Int64; TT=52, ndims=20, thetai=0.38, thetas=0.0044)
     end
 
     # Solve model with standard parameters
-    @time noint    = nopolicy(TT);                                                   # no intervention at all
+    @time noint    = nopolicy(;TT);                                                   # no intervention at all
     @time notest   = withpolicy("notest", nprocs; TT, ndims, thetai, thetas);        # no testing 
     @time untarget = withpolicy("untargettest", nprocs; TT, ndims, thetai, thetas);  # untargeted testing
     @time target   = withpolicy("targettest", nprocs; TT, ndims, thetai, thetas);    # targeted testing
