cdffe4e8333f039a4c33787849e8978157829d67
JulianStreyczek <82290284+JulianStreyczek@users.noreply.github.com>
2022-01-22 18:11:22 +0100

    update 'main' files

--- src/main_parallel.jl
+++ src/main_parallel.jl
@@ -54,10 +54,20 @@ end
 #save("src/data/calib1_target_small.jld2", target);
 #save("src/data/calib1_isolate_small.jld2", isolate);
 
+# Load objects
+#noint = load("src/data/calib1_nointervention.jld2");
+#notest = load("src/data/calib1_notest_small.jld2");
+#untarget = load("src/data/calib1_untarget_small.jld2");
+#target = load("src/data/calib1_target_small.jld2");
+#isolate = load("src/data/calib1_isolate_small.jld2");
+
 # Figures (using freshly-generated data)
 createFig5(noint, notest, untarget)
+#savefig("src/figures/Fig5_dim20")
 createFig6(notest, untarget, target)
+#savefig("src/figures/Fig6_dim20")
 createFig7(untarget, target, isolate)
+#savefig("src/figures/Fig7_dim20")
 
 # Figures (using stored data)
 #createFig5()
