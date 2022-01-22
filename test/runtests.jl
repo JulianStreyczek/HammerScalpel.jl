890f73d9dbcb8ea7ca708c3ce783d3111be9beac
JulianStreyczek <82290284+JulianStreyczek@users.noreply.github.com>
2022-01-22 12:04:20 +0100

    fix unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,16 @@
 using Test, HammerScalpel
 
-@test solveModel(TT=2, ndims=3)==Nothing
+function solveModelTest()
+    
+    # Solve model with standard parameters
+    @time noint    = nopolicy(TT=2, ndims=3);                     # no intervention at all
+    @time notest   = withpolicy("notest", TT=2, ndims=3);         # no testing 
+    @time untarget = withpolicy("untargettest", TT=2, ndims=3);   # untargeted testing
+    @time target   = withpolicy("targettest", TT=2, ndims=3);     # targeted testing
+    @time isolate  = withpolicy("isolate", TT=2, ndims=3);        # isolation
+
+    return("Success")
+
+end
+
+@test solveModelTest()=="Success"
